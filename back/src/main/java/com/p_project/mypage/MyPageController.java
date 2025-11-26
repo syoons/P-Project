f973e0fae282fd5ca123cb1bd1d9b87fab2670ae
yeji-dev25 <jyjjj0329@gachon.ac.kr>
2025-11-27 02:24:25 +0900

    refactor: 요청으로 userId 받는부분 다 삭제 -> jwt에서 가져오는것으로 변경

--- back/src/main/java/com/p_project/mypage/MyPageController.java
+++ back/src/main/java/com/p_project/mypage/MyPageController.java
@@ -1,11 +1,13 @@
 package com.p_project.mypage;
 
+import com.p_project.oauth2.CustomOAuth2User;
 import com.p_project.profile.ProfileDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,26 +18,27 @@ import org.springframework.web.multipart.MultipartFile;
 public class MyPageController {
 
     private final MyPageService mypageService;
+    private final UserService userService;
 
     @PostMapping(path = "/profile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ProfileDTO> updateProfile(
-            @RequestParam("userId") Long userId,
-            @RequestPart("file") MultipartFile file) {
-
-        ProfileDTO result = mypageService.updateProfile(userId, file);
+            Authentication auth,
+            @RequestPart("file") MultipartFile file) throws FileUploadException {
+        CustomOAuth2User principal = (CustomOAuth2User) auth.getPrincipal();
+        ProfileDTO result = mypageService.updateProfile(principal.getUserId(), file);
         return ResponseEntity.ok(result);
     }
 
-    @GetMapping(path = "/{userId}")
-    public ResponseEntity<MyPageDTO> getMyPage(
-            @RequestParam("userId") Long userId) {
-
-        MyPageDTO result = mypageService.getMyPage(userId);
+    @GetMapping
+    public ResponseEntity<MyPageDTO> getMyPage(Authentication auth) {
+        CustomOAuth2User principal = (CustomOAuth2User) auth.getPrincipal();
+        MyPageDTO result = mypageService.getMyPage(principal.getUserId());
         return ResponseEntity.ok(result);
     }
 
-    @PostMapping(path = "update/{userId}")
-    public ResponseEntity<Integer> updateMyPage(
+    @PostMapping(path = "/update")
+    public ResponseEntity<String> updateMyPage(
+            Authentication auth,
             @RequestBody MyPageUpdateDTO myPageDTO) {
 
         try {
